f814e9d4a400c07a70444722e02e4f1de9cae5e4
DIMOPOULOS Athanasios <adimopo@netcompany-intrasoft.com>
2023-08-14 14:14:40 +0300

    new

--- src/main/java/com/ouzerakias/app/config/CorsConfig.java
+++ src/main/java/com/ouzerakias/app/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://192.168.1.6:3000", "http://49.13.24.108:8000")
+                .allowedOrigins("http://localhost:3000", "http://192.168.1.6:3000", "http://49.13.24.108:8000", "http://49.13.24.108:80", "http://49.13.24.108")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Authorization")
